54564f517b68815d6d2b16cc770c9a2330e014c2
nomorechokedboy <haiduongit@protonmail.com>
2023-06-27 13:11:37 +0700

    fix: fixed import errors

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,17 @@
 // Reexport your entry components here
+export { default as Burger } from './components/Burger.svelte';
+export { default as Banner } from './components/Banner.svelte';
+export { default as Button } from './components/Button/Button.svelte';
+export { default as Collapse } from './components/Collapse/Collapse.svelte';
+export { default as DayAtOurSchool } from './components/DayAtOurSchool.svelte';
+export { default as FAQ } from './components/FAQ.svelte';
+export { default as Facilities } from './components/Facilities.svelte';
+export { default as Footer } from './components/Footer.svelte';
+export { default as Foundation } from './components/Foundation.svelte';
+export { default as Header } from './components/Header.svelte';
+export { default as QuestionAndAnswer } from './components/QuestionAndAnswer/QuestionAndAnswer.svelte';
+export { default as Reason } from './components/Reason/Reason.svelte';
+export { default as Schedule } from './components/Schedule/Schedule.svelte';
+export { default as SectionTitle } from './components/SectionTitle/SectionTitle.svelte';
+export { default as TrialForm } from './components/TrialForm/TrialForm.svelte';
+export { default as WhyChooseUs } from './components/WhyChooseUs.svelte';
